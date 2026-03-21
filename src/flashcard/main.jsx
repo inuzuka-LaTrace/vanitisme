27f3f0c695b7d230fce65aec79bc6924567dd7f9
Inuzuka <inuzuka8080@gmail.com>
2026-03-21 17:01:47 +0900

    Create main.jsx

---
+++ src/flashcard/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FlashcardApp from './App';
+import '../index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FlashcardApp />
+  </React.StrictMode>
+);
